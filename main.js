3c619fd27871044d5a64ed55dd91294ff0d57412
anishkumar2023 <132567383+anishkumar2023@users.noreply.github.com>
2023-08-19 10:11:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -40,3 +40,41 @@
                             console.log(i)
                         }
                     }
+
+
+
+                    // assending order
+
+                    let a=[3,7,2,1,9,8]
+
+                    for(i=0;i<a.length;i++){
+                        for(j=i+1;j<a.length;j++){
+                            if(a[i]>a[j]){
+                                let s
+                                s=a[i]
+                                a[i]=a[j]
+                                a[j]=s
+                            }
+                        }
+                    }
+                    console.log(a)
+
+
+
+                    // decending order
+
+
+
+                    let des=[3,7,2,1,9,8]
+
+                    for(i=0;i<des.length;i++){
+                        for(j=i+1;j<des.length;j++){
+                            if(des[i]<des[j]){
+                                let s
+                                s=des[i]
+                                des[i]=des[j]
+                                des[j]=s
+                            }
+                        }
+                    }
+                    console.log(des)
